e63964aefd1197778d1ef0cd9bbed4c12ec9af36
屈文斌 <quwenbin@blchina.com>
2018-11-16 08:49:57 +0800

    接口结构搭建

--- cmd/mega-apiserver/apiserver.go
+++ cmd/mega-apiserver/apiserver.go
@@ -6,9 +6,13 @@ import (
 
 	"github.com/twfx7758/megatron/cmd/mega-apiserver/app"
 	"github.com/twfx7758/megatron/pkg/mega-apiserver/server"
+	"time"
+	"k8s.io/extension/apimachinery/pkg/util/rand"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	command := app.NewAPIServerCommand(server.SetupSignalHandler())
 
 	if err := command.Execute(); err != nil {
--- cmd/mega-apiserver/app/cmd.go
+++ cmd/mega-apiserver/app/cmd.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/spf13/cobra"
+	"fmt"
 )
 
 func NewAPIServerCommand(stopCh <-chan struct{}) *cobra.Command {
@@ -9,17 +10,21 @@ func NewAPIServerCommand(stopCh <-chan struct{}) *cobra.Command {
 		Use:  "mega-apiserver",
 		Long: "对外api",
 		Run: func(cmd *cobra.Command, args []string) {
-			run()
+			run(stopCh)
 		},
 	}
 
 	return cmd
 }
 
-func run() {
+func run(stopCh <-chan struct{}) {
 	host := "127.0.0.1"
 	port := 9100
 
 	router := NewRouter(host, port)
 	router.Start()
+
+	fmt.Printf("mega-apiserver run at %s:%d", host, port)
+
+	<-stopCh
 }
