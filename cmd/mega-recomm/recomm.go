0b22a689b88829d84fe4226ea0bbef7f716988fe
屈文斌 <twfx7758@hotmail.com>
2019-02-17 09:01:06 +0800

    orm查询数据

--- cmd/mega-recomm/recomm.go
+++ cmd/mega-recomm/recomm.go
@@ -1,12 +1,28 @@
 package main
 
 import "github.com/go-xorm/xorm"
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+)
+
+var (
+	conn = "root:qu90()op@tcp(192.168.0.106:3306)/pholcus?charset=utf8"
+)
 
 func main() {
-	engine, err := xorm.NewEngine("mysql", "")
+	engine, err := xorm.NewEngine("mysql", conn)
 	if err != nil {
 		panic(err)
 	}
 	defer engine.Close()
 
+	results, err := engine.Query("SELECT * FROM business__house_detail ORDER BY 1 DESC LIMIT 10")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, row := range results {
+		fmt.Println(row)
+	}
 }
