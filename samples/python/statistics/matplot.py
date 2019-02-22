e4004a4216ad8e7463dacb0056ac1147d9b9813e
屈文斌 <quwenbin@blchina.com>
2019-02-22 13:17:51 +0800

    python读取文件的脚本

--- samples/python/statistics/matplot.py
+++ samples/python/statistics/matplot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 df = pd.read_excel("../testdata/plot.xlsx")
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 步骤一（替换sans-serif字体）
@@ -11,11 +12,27 @@ def zhifangtu():
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.hist(df['Age'], bins=7)
+
     plt.title('Age distribution')
     plt.xlabel('Age')
     plt.ylabel('#Employee')
     plt.show()
 
 
+# 箱线图
+def boxplot():
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.boxplot(df['Age'])
+    plt.show()
+
+
+def violin():
+    sns.violinplot(df['Age'], df['Gender'])
+    sns.despine()
+
+
 if __name__ == "__main__":
-    zhifangtu()
+    # zhifangtu()
+    # boxplot()
+    violin()
