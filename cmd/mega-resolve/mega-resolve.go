89d0ece650a3f5b9fd555aaf4471430c1131e23a
屈文斌 <quwenbin@blchina.com>
2019-07-31 13:30:08 +0800

    python读取文件的脚本

--- cmd/mega-resolve/mega-resolve.go
+++ cmd/mega-resolve/mega-resolve.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
+
+func chain(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("first invoke!")
+
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	return
+	mux := http.NewServeMux()
+	mux.Handle("/echo", chain(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("second invoke!")
+	})))
+
+	http.ListenAndServe("0.0.0.0:9001", mux)
 }
